f73486042f780cec74723e6c4357de8720f2ff6e
haiwu <40200668+USTColumbia@users.noreply.github.com>
2018-06-27 00:15:22 +0800

    Add files via upload
    检测左侧耻骨拐角代码，配置python3.5+opencv2.4.9

---
+++ match_left_corner.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+
+for i in range(1,100):
+	
+    img_rgb = cv2.imread('/media/hai/AIDL-USTC/USTCPro/original/' + str(i) + '.jpeg')
+    img_orinal = cv2.imread('/media/hai/AIDL-USTC/USTCPro/original/' + str(i) + '.jpeg')
+    j = 1
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+    template = cv2.imread('/home/hai/python_work/match_template/templeclub/005.png',0)
+    w, h = template.shape[::-1]
+    print(w)
+    print(h)
+    res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
+    threshold = 0.6
+    a = []
+    b = []
+    loc = np.where( res >= threshold)
+
+    for pt in zip(*loc[::-1]):
+		
+          
+        
+
+        if (((pt[0] + pt[0] + w)/2 < 500) and ((pt[1] + pt[1] + h)/2 < 500) and ((pt[0] + pt[0] + w)/2 > 200) and ((pt[1] + pt[1] + h)/2 > 200) ) :
+            a.append((pt[0] + pt[0] + w)/2)
+            b.append((pt[1] + pt[1] + h)/2)
+           
+        
+            cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 1)
+            print(a[j-1])
+            print(b[j-1])
+            print(j) 
+            j = j + 1
+
+    cv2.imshow('result',img_rgb)
+    print(sum(a)/j,sum(b)/j)
+
+    imageo = cv2.circle(img_orinal,(int(sum(a)/j),int(sum(b)/j)),1,(255,0,255),3)
+    viso = img_orinal.copy()
+    cv2.imwrite('/home/hai/python_work/match_template/resultcornerleft/' + str(i) + '.jpeg',viso)
+    cv2.waitKey(50)
+
