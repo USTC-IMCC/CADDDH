239b43f134642748ee4ddcaea7aafc43e96e4cd6
nonword1 <32115652+nonword1@users.noreply.github.com>
2018-06-27 21:15:52 +0800

    Create point_show.py

---
+++ point_show.py
@@ -0,0 +1,26 @@
+
+import cv2
+
+def point_show(filename):
+    with open(filename) as file_project:
+        content = file_project.read()
+        string = content.split()
+        for i in range(len(string)):
+            if string[i].endswith('.jpg'):
+                tmp_num = i
+                img = cv2.imread(string[i])
+                print img.shape
+                imgname = 'new'+string[i]
+                continue
+            else:
+                if (i-tmp_num)%2 == 1:
+                    x = int(round(float(string[i])))
+                else:
+                    y = int(round(float(string[i])))
+                    img[y,x,2]=255
+                    cv2.circle(img,(x,y), 10, (0,0,255), -1)
+                    cv2.imwrite(imgname,img)
+        
+        
+filename = 'lfm/label.txt'
+point_show(filename)
